b4e77dd1c5b07d25aab06f54970e51190cf7d93c
tetsuwo <tetsuwo.oishi@gmail.com>
2012-06-21 12:47:45 +0900

    modified various files wwww

--- src/js/trello-checker.js
+++ src/js/trello-checker.js
@@ -1,8 +1,13 @@
-TrelloChecker = function() {
-    this.toolbar = chrome.browserAction;
-}
 
-TrelloChecker.prototype.indicate = function(number) {
+var TrelloChecker = {};
+
+(function(Ext) {
+
+Ext.toolbar = chrome.browserAction;
+Ext.timer = 15 * 1000;
+Ext.prevCount = 0;
+
+Ext.indicate = function(number) {
     if (0 < number) {
         this.toolbar.setBadgeText({ text: String(number) });
         this.toolbar.setBadgeBackgroundColor({ color: [200, 0, 0, 255] });
@@ -12,14 +17,53 @@ TrelloChecker.prototype.indicate = function(number) {
     }
 };
 
-TrelloChecker.prototype.log = function(a) {
-    console.log(a);
+Ext.debug = function(val) {
+//    console.log(val);
 };
 
-TrelloChecker.prototype.run = function(init) {
-    if (init) {
-        this.toolbar.setBadgeBackgroundColor({ color: [66, 66, 66, 255] });
-        this.toolbar.setBadgeText({ text: '--' });
+Ext.getCount = function() {
+    var that = Ext;
+    that.debug('start');
+
+    if (!localStorage.token) {
+        that.startInterval();
+    }
+
+    that.run();
+
+    if (Trello.authorized()) {
+        Trello.members.get(
+            'me/notifications/unread',
+            function (response) {
+                that.debug(response);
+
+                if (that.prevCount != response.length) {
+                    that.prevCount = response.length;
+                    that.indicate(response.length);
+                }
+
+                that.startInterval();
+            }
+        );
+    }
+    else {
+        that.startInterval();
     }
 };
 
+Ext.startInterval = function() {
+    window.setTimeout(this.getCount, this.timer);
+};
+
+Ext.run = function(init) {
+    if (localStorage.token) {
+        Trello.setToken(localStorage.token);
+    }
+
+    if (init) {
+        this.indicate('--');
+        this.getCount();
+    }
+};
+
+})(TrelloChecker);
