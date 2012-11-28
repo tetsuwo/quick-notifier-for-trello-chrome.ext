50d3f8ca374059d52dbd953f7e3f2d06ee9f60e8
tetsuwo <tetsuwo.oishi@gmail.com>
2012-11-28 15:43:48 +0900

    added travis-ci sets

--- test/test.js
+++ test/test.js
@@ -1,64 +1,52 @@
-describe('WebsiteBlocker', function() {
 
-    var testdata = {
-    };
-    var blockedArray = [];
+describe('TrelloChecker', function() {
+    var Notif = TrelloChecker;
 
-    it('default', function() {
-        var wb = new WebsiteBlocker();
-
-        expect(wb.blockedList).toBeNull();
-        expect(wb.date).toBeNull();
-        expect(wb.time).toBeNull();
-        expect(wb.debug).toBeFalsy();
-        expect(wb.disabledTimeLimit).toBeFalsy();
+    it('default: url', function() {
+        expect(Notif.url).toEqual('https://trello.com');
     });
 
-    it('isBlocked', function() {
-        var wb = new WebsiteBlocker();
-        wb.debug = true;
-        wb.disabledTimeLimit = true;
-
-        expect(wb.debug).toBeTruthy(
-            wb.isBlocked('http://www.yahoo.co.jp', 'yahoo\.co\.jp', [])
-        );
+    it('default: timer', function() {
+        expect(Notif.timer).toEqual(15000);
     });
 
-    it('toFormat', function() {
-        var wb = new WebsiteBlocker();
-        wb.debug = true;
-        wb.disabledTimeLimit = true;
-
-        var text = '';
-        text += '(facebook|twitter).com 0000-0730,0945-1159\n';
-        text += 'yahoo.co.jp';
-        var data = blockedArray = wb.toFormat(text);
-        console.log(data);
-
-        expect(data.length).toEqual(2);
-
-        expect(data[0].url).toEqual('(facebook|twitter).com');
-        expect(data[0].regexp).toEqual('(facebook|twitter)\\.com');
-        expect(data[0].time.length).toEqual(2);
-
-        expect(data[1].url).toEqual('yahoo.co.jp');
-        expect(data[1].regexp).toEqual('yahoo\\.co\\.jp');
-        expect(data[1].time.length).toEqual(0);
+    it('default: prevCount', function() {
+        expect(Notif.prevCount).toBe(null);
     });
 
-    it('toString', function() {
-        var wb = new WebsiteBlocker();
-        wb.debug = true;
-        wb.disabledTimeLimit = true;
-
-        var text = '';
-        text += '(facebook|twitter).com 0000-0730,0945-1159\n';
-        text += 'yahoo.co.jp ';
-
-        var data = wb.toString(blockedArray);
-        console.log(data);
-
-        expect(data).toEqual(text);
+    it('default: toolbar', function() {
+//        expect(Notif.toolbar).toBe(undefined);
     });
 
+    it('getColor: ALERT', function() {
+        expect(Notif.getColor('alert')).toEqual([203, 77, 77, 255]);
+        expect(Notif.getColor('ALERT')).toEqual([203, 77, 77, 255]);
+    });
+
+    it('getColor: CLEAR', function() {
+        expect(Notif.getColor('clear')).toEqual([52, 178, 125, 255]);
+        expect(Notif.getColor('CLEAR')).toEqual([52, 178, 125, 255]);
+    });
+
+    it('getColor: UNKO(not found type)', function() {
+        expect(Notif.getColor('unko')).toEqual([66, 66, 66, 255]);
+        expect(Notif.getColor('UNKO')).toEqual([66, 66, 66, 255]);
+    });
+
+    it('getColor: empty string', function() {
+        expect(Notif.getColor('')).toEqual([66, 66, 66, 255]);
+    });
+
+    it('getColor: null character', function() {
+        expect(function() {
+            Notif.getColor(null);
+        }).toThrow();
+    });
+
+    it('getColor: nothing', function() {
+        expect(function() {
+            Notif.getColor();
+        }).toThrow();
+    });
 });
+
