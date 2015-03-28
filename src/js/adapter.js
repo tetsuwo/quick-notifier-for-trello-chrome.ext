094fcc27ba1480fb819da4f734a78b26e9e9c582
tetsuwo <tetsuwo.oishi@gmail.com>
2015-03-28 15:41:52 +0900

    Improve function notify

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -1,16 +1,5 @@
-/*!
- * Adapter
- *
- * Copyright 2010-2012, Tetsuwo OISHI.
- * Dual license under the MIT license.
- * http://tetsuwo.tumblr.com
- *
- * CreatedAt: 2012-06-21
- */
-
 var Adapter = {};
 
-// Event handlers for the various notification events
 function notifClosed(notID, bByUser) {
     console.log("The notification '" + notID + "' was closed" + (bByUser ? " by the user" : ""));
 }
@@ -23,11 +12,14 @@ function notifButtonClicked(notID, iBtn) {
     console.log("The notification '" + notID + "' had button " + iBtn + " clicked");
 }
 
-(function(obj) {
+// set up the event listeners
+chrome.notifications.onClosed.addListener(notifClosed);
+chrome.notifications.onClicked.addListener(notifClicked);
+chrome.notifications.onButtonClicked.addListener(notifButtonClicked);
 
+(function(obj) {
     obj.url       = 'https://trello.com';
     obj.toolbar   = chrome.browserAction;
-    obj.notif     = chrome.notifications;
     obj.timer     = 15 * 1000;
     obj.prevCount = null;
     obj.responses = {};
@@ -42,13 +34,11 @@ function notifButtonClicked(notID, iBtn) {
     obj.getColor = function(type) {
         switch (type.toUpperCase()) {
             case 'ALERT':
-                return [203, 77, 77, 255];
-
-            case 'CLEAR':
-                return [52, 178, 125, 255];
-
+                return [200, 0, 0, 255];
+            //case 'CLEAR':
+            //    return [66, 178, 125, 255];
             default:
-                return [66, 66, 66, 255];
+                return [150, 150, 150, 255];
         }
     };
 
@@ -60,7 +50,7 @@ function notifButtonClicked(notID, iBtn) {
         var id = this.generateId(this.idPrefix);
         var options = {
             type: 'basic',
-            iconUrl: 'images/128.png',
+            iconUrl: 'images/trello-logo_icon-notify.png',
             title: title,
             message: message
         };
@@ -77,9 +67,9 @@ function notifButtonClicked(notID, iBtn) {
     };
 
     obj.setIcon = function(type) {
-        var iconPath = 'images/128.png';
+        var iconPath = 'images/trello-icon_grayscale.png';
         if (type === 'ALERT') {
-            iconPath = 'images/128.png';
+            iconPath = 'images/trello-icon_colorful.png';
         }
         this.toolbar.setIcon({ path: iconPath });
     };
@@ -91,9 +81,8 @@ function notifButtonClicked(notID, iBtn) {
     };
 
     obj.handleToolbar = function (type) {
-        var type = type.toUpperCase();
         this.setColor(type);
-        //this.setIcon(type);
+        this.setIcon(type);
         if (type === 'ALERT') {
             this.notify(
                 'Trello Notifications',
